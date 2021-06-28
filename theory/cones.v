52c7d9782fb46e93f0086ae41ff193f636d43827
Michael George <mdgeorge@cs.cornell.edu>
2021-06-28 17:59:33 -0400

    finished proof of SemiRingOrder and StrictSemiRingOrder

--- theory/cones.v
+++ theory/cones.v
@@ -1,8 +1,10 @@
 Require Import
   MathClasses.interfaces.abstract_algebra
-  MathClasses.interfaces.orders
+  MathClasses.interfaces.orders MathClasses.orders.rings
   MathClasses.interfaces.cones
+  MathClasses.theory.groups
   MathClasses.theory.strong_setoids
+  MathClasses.theory.rings Coq.setoid_ring.Ring
   MathClasses.misc.group_automation.
 
 (**
@@ -17,6 +19,9 @@ Section relation_from_cone.
 
   Lemma cone_rel_compat_right : ∀ x y z, x ~ y -> x & z ~ y & z.
   Proof. intros; unfold cone_relation; group_simplify; easy. Qed.
+
+  Lemma cone_rel_compat_left `{!AbGroup A}: ∀ x y z, x ~ y -> z & x ~ z & y.
+  Proof. intros; unfold cone_relation; repeat rewrite (commutativity z); group_simplify; easy. Qed.
 
   (* TODO: is this the proper use of Proper? *)
   Global Instance: ∀ z, Proper ((~) ==> (~)) (& z).
@@ -50,16 +55,15 @@ Section le_from_nonneg.
   Context `{Group A}
           `{!GroupCone cone} `{!WeakCone cone}.
 
-  Local Instance: IsNonNeg A := cone.
-  Local Existing Instance cone_rel_is_le | 0.
+  Local Notation "(≤)" := (cone_relation cone).
 
   Global Instance : Reflexive (≤).
-  Proof. repeat red; fold is_nonneg; intros; group_simplify; exact wcone_weak. Qed.
+  Proof. repeat red; intros; group_simplify; exact wcone_weak. Qed.
 
   Global Instance cone_le_is_po : PartialOrder (≤).
   Proof. repeat (split; try apply _). Qed.
 
-  Context `{!TotalCone is_nonneg}.
+  Context `{!TotalCone cone}.
 
   Global Instance : TotalRelation (≤).
   Proof. repeat red; intros.
@@ -78,13 +82,12 @@ Section lt_from_pos.
   Context `{Group A}
           `{!GroupCone cone} `{!StrictCone cone}.
 
-  Local Instance: IsPos A := cone.
-  Local Existing Instance cone_rel_is_lt | 0.
+  Local Notation "(<)" := (cone_relation cone).
 
   Global Instance : Irreflexive (<).
   Proof. repeat red; intros x equiv;
     apply scone_strict;
-    unfold lt, cone_rel_is_lt, cone_relation in equiv;
+    unfold lt, cone_relation in equiv;
     rewrite right_inverse in equiv;
     assumption.
     Qed.
@@ -92,11 +95,11 @@ Section lt_from_pos.
   Global Instance: StrictSetoidOrder (<).
   Proof. repeat (split; try apply _). Qed.
 
-  Context `{!TotalCone is_pos} `{∀ x y, Decision (x = y)}.
+  Context `{!TotalCone cone} `{∀ x y, Decision (x = y)}.
 
   Global Instance: Trichotomy (<).
   Proof.
-    red. unfold lt, cone_rel_is_lt, cone_relation; intros.
+    red. unfold cone_relation; intros.
     setoid_replace (y & -x) with (-(x & -y)) by group.
     destruct (tcone_total (x & -y)) as [lt | [gt | eq]].
       right; right; assumption.
@@ -106,11 +109,11 @@ Section lt_from_pos.
 
   Global Instance: CoTransitive (<).
   Proof. red. intros.
-    destruct (trichotomy (<) x z) as [lt | [ eq | gt ] ];
-      [ left; assumption
-      | right; rewrite <- eq; assumption
-      | right; transitivity x; assumption
-      ].
+    destruct (trichotomy (<) x z) as [lt | [ eq | gt ] ].
+      left; assumption.
+      right. assert (Symmetric equiv). typeclasses eauto.
+      rewrite <- eq. assumption.
+      right; transitivity x; assumption.
   Qed.
 
   Global Instance: PseudoOrder (<).
@@ -124,8 +127,8 @@ Section lt_from_pos.
         contradiction.
         right; assumption.
     intros ineq. destruct ineq as [lt | gt].
-    repeat red. intros eq. rewrite eq in lt. apply scone_strict. repeat red in lt. fold is_pos in lt. rewrite right_inverse in lt. assumption.
-    repeat red. intros eq. rewrite eq in gt. apply scone_strict. repeat red in gt. fold is_pos in gt. rewrite right_inverse in gt. assumption.
+    repeat red. intros eq. rewrite eq in lt. apply scone_strict. repeat red in lt. rewrite right_inverse in lt. assumption.
+    repeat red. intros eq. rewrite eq in gt. apply scone_strict. repeat red in gt. rewrite right_inverse in gt. assumption.
     Qed.
 
 End lt_from_pos.
@@ -133,20 +136,50 @@ End lt_from_pos.
 (*******************************************************************************
  * The relation R induced by a ring cone C is a ring order.
  *)
-(*
+
+Section rcone_closed.
+  Local Existing Instance plus_is_sg_op | 0.
+  Context `{Ring A} `{!RingCone cone}.
+  Add Ring R : (stdlib_ring_theory A).
+
+  Local Infix "~" := (cone_relation cone) (at level 70, no associativity).
+  Local Notation "(~)" := (cone_relation cone).
+
+  Lemma foo : ∀ x, x - 0 = x.
+  Proof. intros. ring. Qed.
+
+  Lemma mult_spec : ∀ x y, PropHolds (0 ~ x) -> PropHolds (0 ~ y) -> PropHolds (0 ~ x * y).
+  Proof. unfold PropHolds; unfold cone_relation, sg_op, plus_is_sg_op. fold plus.
+    intros. rewrite foo in *. apply sgcone_sgop; assumption.
+  Qed.
+End rcone_closed.
+
 Section ring_order_from_rcone.
-  Context `{Ring A} `{!RingCone cone} `{!WeakCone cone}.
+  Local Existing Instance plus_is_sg_op | 0.
+  Context `{Ring A} `{!WeakCone cone} `{!RingCone cone}.
 
   Local Instance: IsNonNeg A := cone.
   Local Existing Instance cone_rel_is_le | 0.
   
-  Lemma plus_spec: ∀ z, OrderPreserving (z +).
-  Proof. intros. split. split. apply _. Print Instances PartialOrder. split. apply _. unfold le. apply _. apply _.
+  Lemma weak_plus_spec: ∀ z, OrderPreserving (z +).
+  Proof. repeat (split; try apply _); intros; apply cone_rel_compat_left; easy. Qed.
 
-Instance: 
-End RingConeOrders.
+  Instance: SemiRingOrder (≤).
+  Proof. apply from_ring_order. apply weak_plus_spec. apply mult_spec. Qed.
+End ring_order_from_rcone.
 
-Section OrderCones.
+Section strict_ring_order_from_rcone.
+  Local Existing Instance plus_is_sg_op | 0.
+  Context `{Ring A} `{!StrictCone cone} `{!RingCone cone}.
 
-End OrderCones.
-*)
+  Local Instance: IsPos A := cone.
+  Local Existing Instance cone_rel_is_lt | 0.
+
+  Local Lemma strict_plus_spec : ∀ z, StrictlyOrderPreserving (z +).
+  Proof. repeat (split; try apply _); intros; apply cone_rel_compat_left; easy. Qed.
+
+  Lemma from_strict_ring_order: StrictSemiRingOrder (<).
+  Proof. apply from_strict_ring_order. apply strict_plus_spec. apply mult_spec. Qed.
+
+End strict_ring_order_from_rcone.
+
