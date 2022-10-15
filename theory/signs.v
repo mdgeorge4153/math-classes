7041f6ad513875bca4d81b642552f4a5a3ca9d40
Michael George <michael@certora.com>
2022-10-15 12:06:57 -0400

    checked transitivity of < from positivity

--- theory/signs.v
+++ theory/signs.v
@@ -9,9 +9,17 @@ Require Import
 
 Section relation_from_positivity.
 
-Context `{Group A}.
+Context `{PositiveGroup A}.
 
 #[global]
-Instance positivity_is_le `{Positive A}: Le A := λ x y, positive (y & (-x)).
+Instance positivity_is_lt: Lt A := λ x y, positive (y & (-x)).
+
+#[global]
+Instance: Transitive (<).
+Proof. unfold lt, positivity_is_lt; intros x y z;
+    setoid_replace (z & -x) with ((z & -y) & (y & -x)) by group;
+    intros; apply psg_sgop; easy.
+Qed.
 
 End relation_from_positivity.
+
